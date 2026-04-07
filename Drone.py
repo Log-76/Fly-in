e9bc17e725aaaa13b3c97602cb5eb52d30b224b1
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-04-07 17:19:48 +0200

    Drone

--- Drone.py
+++ Drone.py
@@ -1,11 +1,18 @@
 import Zone
-
+import random
 
 class Drone():
     def __init__(self, id: str, hub: Zone, target: Zone):
         self.id = id
-        self.hub = hub
+        self.hub: Zone = hub
         self.target = target
         self.path: list[Zone] = []
-    
+
     def move(self) -> None:
+        if self.target != self.hub:
+            self.path.append(self.hub)
+            temp = random(self.hub.adjacent)
+            if temp.can_enter() is True:
+                self.hub.drone_current.pop()
+                self.hub = temp
+                self.hub.drone_current.append(self.id)
