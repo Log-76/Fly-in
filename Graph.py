0b4240b754a8fd7281c37e1ee8f38a41cbdfaba8
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-04-08 17:39:56 +0200

    graph

--- Graph.py
+++ Graph.py
@@ -12,16 +12,24 @@ class Graph():
         self.drones: list[Drone] = []
 
     def load_hub(self) -> None:
+        """this fonction create all hub need"""
         for temps in self.data["hub"]:
             self.hub[temps[0]] = Zone(*temps)
 
     def load_connex(self) -> None:
+        """this fonction create all connexion in data"""
         for temps in self.data["connexion"]:
             self.connexion.append(Connexion(self.hub[temps[0]],
                                             self.hub[temps[1]], temps[2]))
 
     def load_drones(self) -> None:
+        """this fonction create all drone need"""
         for temps in range(self.data["drone"]):
             self.drones.append(Drone(temps, self.hub[self.data["start_hub"]],
                                      self.hub[self.data["end_hub"]]))
 
+    def setup(self) -> None:
+        """this metho activate all fonction"""
+        self.load_hub()
+        self.load_connex()
+        self.load_drones()
