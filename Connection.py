adb6e08885cd74b6caf8381f015e1a97b6023d41
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-04-08 14:54:55 +0200

    connexion

--- Connection.py
+++ Connection.py
@@ -2,7 +2,9 @@ from Zone import Zone
 
 
 class Connexion():
-    def __init__(self, hub_a: Zone, hub_b: Zone, meta) -> None:
+    def __init__(self, hub_a: Zone, hub_b: Zone, meta: dict) -> None:
         self.hub_a = hub_a
         self.hub_b = hub_b
         self.meta = meta
+        self.hub_a.add_neighbor(self.hub_b)
+        self.hub_b.add_neighbor(self.hub_a)
